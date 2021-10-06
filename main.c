f07397ce7eed8166a2fe9467908b7be6fedb0161
Teissier Yannis <yannis.teissier70@gmail.com>
2021-10-06 09:47:54 +0200

    'init'

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* TODO
+ * tri à bulle
+ * tri par selection
+ * tri par insertion
+ * tri pas tas
+ * permettant un tri croissant ou décroissant d'un tableau de float
+ *
+ *
+ * fichier de benchmark en fonction de différentes tailles de tableaux
+ *
+ * faire une sortie en fichier
+ *
+ *
+ * Écrire le fichier README.md décrivant l’objet du projet, l’usage des commandes, les résultats attendus et les évolutions à venir.
+ *
+ *
+ * - Les tableaux avant tri doivent être les mêmes pour chaque algorithme testé (cf notion de nombre pseudo aléatoire et de graine)
+ * - Chaque algorithme devra être testé 3 fois avec des tableaux différents. Vous ne garderez que la valeur moyenne des trois tests.
+ * Vous devez trier des tableaux contenant des valeurs aléatoires comprises entre 0 et 10^6
+ * Vous devez réaliser les tests avec des tableaux contenants respectivement 100, 10^3, 10^4, 10^5, 10^6, 10^7 valeurs
+ */
+int main() {
+    return EXIT_SUCCESS;
+}
